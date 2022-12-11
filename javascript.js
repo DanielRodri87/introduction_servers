e3007e58eb2a97f76afc15a96124c3b3ff4f5d4b
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2022-12-11 10:56:12 -0300

    att

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
-// Se o botão click for clicado, gerar um alert, com o texto "Hello World"
-
-if (document.getElementById("click")) {
-    document.getElementById("click").addEventListener("click", function() {
-        alert("Hello World");
+function myfunction() {
+    document.getElementById("click").addEventListener("click", function(){
+        window.alert("clicou");
     });
 }
